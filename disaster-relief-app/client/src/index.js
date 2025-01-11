42ebc0bb7dca0b51e8bd681e6a3093b2f37b7a7e
tony1234567891011 <130993859+tony1234567891011@users.noreply.github.com>
2025-01-11 14:29:29 -0500

    Added Login, broken API

--- disaster-relief-app/client/src/index.js
+++ disaster-relief-app/client/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Updated import
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root')); // Use createRoot
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
